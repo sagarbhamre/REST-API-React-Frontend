76b1e826c29689abea81de5642d8372f08ed3ebe
sagarbhmr@gmail.com <sagarbhr@gmail.com>
2025-02-02 19:55:27 +0530

    socket connection established

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -40,7 +40,28 @@ class Feed extends Component {
       .catch(this.catchError);
 
     this.loadPosts();
-    openSocket('http://localhost:8080')
+    const socket = openSocket('http://localhost:8080');
+    socket.on('posts', data => {
+      if(data.action === 'create') {
+          this.addPost(data.post); // <=== received socket data will be passed to frontend for display
+      }
+    })
+  }
+
+  addPost = post => {
+    this.setState(prevState => {
+      const updatedPosts = [...prevState.posts];
+      if (prevState.postPage === 1) {
+        if (prevState.posts.length >= 2) {
+          updatedPosts.pop();
+        }
+        updatedPosts.unshift(post);
+      }
+      return {
+        posts: updatedPosts,
+        totalPosts: prevState.totalPosts + 1
+      };
+    });
   }
 
   loadPosts = direction => {
